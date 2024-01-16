503303515288ef3c35ee38492a0d4630554c303e
Sukhmangill977 <sukhmangill977@gmail.com>
2024-01-16 23:54:31 +0530

    2

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -8,6 +8,9 @@ import DoctorDetails from '../pages/Doctors/DoctorDetails'
 import MyAccount from '../Dashboard/user-account/MyAccount'
 import{Routes,Route} from 'react-router-dom'
 import Dashboard from '../Dashboard/doctor-account/Dashboard'
+
+import ProtectedRoute from './ProtectedRoute'
+
 const Routers = () => {
 
   return ( 
@@ -20,8 +23,8 @@ const Routers = () => {
     <Route path="/register" element={<Signup/>} /> 
     <Route path="/contact" element={<Contact/>} /> 
     <Route path="/services" element={<Services/>} />  
-    <Route path="/users/profile/me" element={<MyAccount/>} /> 
-    <Route path="/doctors/profile/me" element={<Dashboard/>} />   
+    <Route path="/users/profile/me" element={<ProtectedRoute allowedRoles={["patient"]}><MyAccount/></ProtectedRoute>} /> 
+    <Route path="/doctors/profile/me" element={<ProtectedRoute allowedRoles={["doctor"]}><Dashboard/></ProtectedRoute>  } />
     
     </Routes>
   )
