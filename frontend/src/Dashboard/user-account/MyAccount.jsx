263c90b6c9a5d0c1fb294303eb9d462cd2568359
Sukhmangill977 <sukhmangill977@gmail.com>
2024-01-17 18:25:32 +0530

    qwerty4

--- frontend/src/Dashboard/user-account/MyAccount.jsx
+++ frontend/src/Dashboard/user-account/MyAccount.jsx
@@ -30,7 +30,7 @@ const MyAccount = () => {
             <p className="text-textColor text-[15px] leading-6 font-medium">
               Blood Type:
               <span className="ml-2 text-headingColor text-[22px] leading-8">
-                O
+                O-
               </span>
             </p>
           </div>
