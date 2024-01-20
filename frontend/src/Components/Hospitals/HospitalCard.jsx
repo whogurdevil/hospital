d088d583f306b2569f35290f6d88545ea4bed706
Sukhmangill977 <sukhmangill977@gmail.com>
2024-01-20 12:33:26 +0530

    updating hospital file

--- frontend/src/Components/Hospitals/HospitalCard.jsx
+++ frontend/src/Components/Hospitals/HospitalCard.jsx
@@ -5,26 +5,15 @@ import { BsArrowRight } from "react-icons/bs";
 const HospitalCard = ({ hospital }) => {
   const {
     name,
-    email,
-    averageRating,
+    avgRating,
     totalRating,
     photo,
     specializaton,
     totalPatients,
     hospital,
-    qualifications
   } = doctor;
   return (
     <div className="p-3 lg:p-5">
-      <div>
-        <span>{name}</span>
-      </div>
-      <div>
-        <span>{qualifications}</span>
-      </div>
-      <div>
-        <span>{email}</span>
-      </div>
       <div>
         <img src={photo} className="w-full" alt="" />
       </div>
@@ -40,10 +29,12 @@ const HospitalCard = ({ hospital }) => {
           {specializaton}
         </span>
         
+        
+
         <div className="flex items-center gap-[6px]">
           <span className="flex items-center gap-[6px] text-[14px] leading-6 lg:text-[16px] lg:leading-7
             font-semibold text-headingColor">
-            <img src={starIcon} alt="" /> {averageRating}
+            <img src={starIcon} alt="" /> {avgRating}
           </span>
           <span className="text-[14px] leading-6 lg:text-[16px] lg:leading-7 font-[400] text-textColor">
             ({totalRating})
@@ -63,7 +54,7 @@ const HospitalCard = ({ hospital }) => {
           </p>
         </div>
         <Link
-          to="/doctors/3"
+          to="/doctors"
           className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E]  
           flex items-center justify-center group hover:bg-primaryColor hover:border-none">
           <BsArrowRight className="group-hover:text-white w-6 h-5" />
@@ -72,4 +63,4 @@ const HospitalCard = ({ hospital }) => {
     </div>
   );
 };
-export default DoctorCard;
+export default HospitalCard;
--- frontend/src/Components/Hospitals/HospitalList.jsx
+++ frontend/src/Components/Hospitals/HospitalList.jsx
@@ -1,34 +1,16 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import HospitalCard from './HospitalCardCard';
 
-const DoctorsList = () => {
-  const [doctors, setDoctors] = useState([]);
+import { hospitals } from '../../assets/data/hospital';
+import HospitalCard from './HospitalCard'
 
-  useEffect(() => {
-    const fetchDoctors = async () => {
-      try {
-        const response = await axios.get('http://localhost:5001/api/v1/doctors');
-
-        setDoctors(response.data.data);
-   // Assuming your API response is an array of doctors
-      } catch (error) {
-        console.error('Error fetching doctors:', error);
-      }
-    };
-
-    fetchDoctors();
-  }, []); // Empty dependency array ensures the effect runs once on component mount
-
+const HospitalsList = () => {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 lg:gap[30px] mt-[30px] lg:mt-[55px]'>
-    <h1>hello </h1>
-      {doctors.map((doctor) => (
-        <DoctorCard key={doctor.id} doctor={doctor} />
-      ))}
-      {console.log(doctors)}
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 lg:gap[30px] mt-[30px] lg:mt-
+    [55px]'>{hospitals.map((hospital)=>(
+         <HospitalCard key={hospital.id} hospital={hospital}/> 
+         ))}
+      
     </div>
-  );
-};
+  )
+}
 
-export default DoctorsList;
+export default HospitalsList
