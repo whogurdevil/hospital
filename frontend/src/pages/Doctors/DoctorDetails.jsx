3f0b030b9598c501d9a2f766d221b15327376518
Sukhmangill977 <sukhmangill977@gmail.com>
2024-01-21 05:25:20 +0530

    QW

--- frontend/src/pages/Doctors/DoctorDetails.jsx
+++ frontend/src/pages/Doctors/DoctorDetails.jsx
@@ -19,10 +19,11 @@ const DoctorDetails = () => {
               <div>
                 <span className="bg-[#CCF0F3] text-irisBlueColor py-1 px-6 lg:py-2 lg:px-6 text-[12px]
                 leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded">
-                  Surgeon
+                   LABOR AND DELIVERY
+
                 </span>
                 <h3 className="text-headingColor text-[22px] leading-9 mt-3 font-bold">
-                  Muhibur Rahman
+                  DR RAJ PATEL
                 </h3>
                 <div className="flex items-center gap-[6px]">
                   <span className="flex items-center gap-[6px] text-[14px] leading-5 lg:text-[16px]
@@ -35,8 +36,8 @@ const DoctorDetails = () => {
                   </span>
                 </div>
                 <p className="text__para text-[14px] leading-6 md:text-[15px] lg:max-w-[390px]">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Dicta, alias!
+                MBBS LABOR AND DELIVERY
+
                 </p>
               </div>
             </div>
